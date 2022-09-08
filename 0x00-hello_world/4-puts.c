908021e1c636003119c9af308aea40439e76b37a
Orezime <orezime.igata@gmail.com>
2022-09-08 11:46:10 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: 0 if success
- * */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Porogramming is like building a multilingual puzzle");
 	return (0);
-	}
+} 
