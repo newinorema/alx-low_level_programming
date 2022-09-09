8877ecc102e0c51da1e2c5990a2295efbfeeab9a
Orezime <orezime.igata@gmail.com>
2022-09-08 20:28:14 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - main block
- *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
