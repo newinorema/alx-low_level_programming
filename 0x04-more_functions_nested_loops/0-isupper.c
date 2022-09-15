e9a7988384e64aa911713884113d32cc27d70f2f
Orezime <orezime.igata@gmail.com>
2022-09-15 07:14:50 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+i#include "main.h"
+
+/**
+ * _isupper -> this is a function to check upper case alphas
+ * @c: an integer parameter
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
