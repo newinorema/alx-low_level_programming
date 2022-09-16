334aa4e895b200d0918f535af8319fb37995dbf2
Orezime <orezime.igata@gmail.com>
2022-09-16 10:15:09 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,37 +1,19 @@
 #include "main.h"
 
-
-
 /**
- *
- * * print_alphabet_x10 -> prints the lowercase alphabets
- *
- * */
-
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
 void print_alphabet_x10(void)
-
 {
+	int i;
+	int n;
 
-		int i;
-
-			int n;
-
-
-
-				for (n = 0; n < 10; n++)
-
-						{
-
-									for (i = 'a'; i <= 'z'; i++)
-
-												{
-
-																_putchar(i);
-
-																		}
-
-											_putchar('\n');
-
-												}
-
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
