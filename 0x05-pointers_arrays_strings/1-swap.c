8cb667209eb17962580570337186d5b9c25ee898
Orezime <orezime.igata@gmail.com>
2022-09-19 11:29:54 -0700

    10. Great leaders are willing to sacrifice the numbers...

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - Swaps the values of two integers.
- * @a: The first integer to be swapped.
- * @b: The second integer to be swapped.
+ * swap_int - a function that swaps the values of two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: integers
  */
 void swap_int(int *a, int *b)
 {
-	int tmp = *a;
+	int n;
+
+	n = *a;
 	*a = *b;
-	*b = tmp;
+	*b = n;
 }
